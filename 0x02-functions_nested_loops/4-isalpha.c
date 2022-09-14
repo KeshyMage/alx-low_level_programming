bcd75c1485d03b2d28a69711204ecd593ee0799d
KeshyMage <ogunniranstephen@gmail.com>
2022-09-14 04:15:48 -0700

    My commit error rectify

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,9 @@
 /**
  * _isalpha - a function that checks for alphabetic character
  * @c: single letter input
- * Return 1 if c is a letter (lower or uppercase), 0 otherwise
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
+
 int _isalpha(int c)
 {
 	if (((c >= 65) && (c <= 90)) || ((c >= 97) && (c <= 122)))
